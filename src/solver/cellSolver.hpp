7a3e31d23950bb9c3d6f04aa18c7fc6418ac75ec
Joseph Marziale <jjmarzia@compile2.core.ccr.buffalo.edu>
2024-11-04 08:06:18 -0500

    second attempt merge salac/EOSfix into intsharp/surfaceforce

--- src/solver/cellSolver.hpp
+++ src/solver/cellSolver.hpp
@@ -43,14 +43,15 @@ class CellSolver : public solver::Solver {
     //! list of auxField update functions
     std::vector<SolutionFieldUpdateFunctionDescription> solutionFieldUpdateFunctionDescriptions;
 
-   protected:
+   public:
+//   protected:
     //! Vector used to describe the entire cell geom of the dm.  This is constant and does not depend upon region.
     Vec cellGeomVec = nullptr;
 
     //! Vector used to describe the entire face geom of the dm.  This is constant and does not depend upon region.
     Vec faceGeomVec = nullptr;
 
-   public:
+//   public:
     /**
      * Create a base solver used for cell based solvers
      * @param solverId
