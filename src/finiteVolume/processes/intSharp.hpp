12099649381961a96465fb536ea50c83aae6aba5
jjmarzia <jjmarzia@Lalo-6.local>
2025-02-19 09:09:11 -0500

    temp switch back to my code
    intsharp0 is now the prestage-modified salac version

--- src/finiteVolume/processes/intSharp.hpp
+++ src/finiteVolume/processes/intSharp.hpp
@@ -9,8 +9,7 @@
 #include "flowProcess.hpp"
 #include "process.hpp"
 #include "solver/solver.hpp"
-#include "finiteVolume/processes/twoPhaseEulerAdvection.hpp"
-#include "finiteVolume/stencils/gaussianConvolution.hpp"
+#include "twoPhaseEulerAdvection.hpp"
 
 namespace ablate::finiteVolume::processes {
 
@@ -20,38 +19,10 @@ class IntSharp : public Process {
     //coeffs
     PetscReal Gamma;
     PetscReal epsilon;
-    //need a more permanent fix since the user wouldn't know how to use this (or we can just do default true) but
-    //optionally add the intsharp term to the RHS of the densityVFField equation 
-    bool addToRHS;
-
-
-    DM cellDM = nullptr;
-    DM fluxDM = nullptr;
-    DM vertDM = nullptr;
-    std::shared_ptr<ablate::finiteVolume::stencil::GaussianConvolution> cellGaussianConv = nullptr;
-    std::shared_ptr<ablate::finiteVolume::stencil::GaussianConvolution> vertexGaussianConv = nullptr;
-    enum VecLoc { LOCAL , GLOBAL };
-    const PetscReal phiRange[2] = {1.e-4, 1.0 - 1.e-4};
-
-    void ClearData();
-
-    void SetMasks();
-
-    struct vecData {
-      DM dm;
-      Vec vec;
-      PetscScalar *array;
-    };
-
-    std::vector<struct vecData> localVecList = {};
-    std::vector<struct vecData> globalVecList = {};
-
-    void MemoryHelper(DM dm, VecLoc loc, Vec *vec, PetscScalar **array);
-    void MemoryHelper();
-
-    void SetMasks(ablate::domain::Range &cellRange, DM phiDM, Vec phiVec, PetscInt phiID, Vec cellMaskVec[2], PetscScalar *cellMaskArray[2], PetscScalar *vertMaskArray);
-
-
+    bool flipPhiTilde;
+    //mesh for vertex information
+    DM vertexDM{};
+    std::shared_ptr<ablate::domain::SubDomain> subDomain;
 
    public:
     /**
@@ -59,7 +30,7 @@ class IntSharp : public Process {
      * @param Gamma
      * @param epsilon
      */
-    explicit IntSharp(PetscReal Gamma, PetscReal epsilon, bool addToRHS = true);
+    explicit IntSharp(PetscReal Gamma, PetscReal epsilon, bool flipPhiTilde);
 
     /**
      * Clean up the dm created
@@ -84,16 +55,6 @@ class IntSharp : public Process {
      * @return
      */
     static PetscErrorCode ComputeTerm(const FiniteVolumeSolver &solver, DM dm, PetscReal time, Vec locX, Vec locFVec, void *ctx);
-
-
-    //intsharp prestage stuff
-    inline const static std::string VOLUME_FRACTION_FIELD = eos::TwoPhase::VF;
-    inline const static std::string DENSITY_VF_FIELD = ablate::finiteVolume::CompressibleFlowFields::CONSERVED + VOLUME_FRACTION_FIELD;
-    PetscErrorCode PreStage(TS flowTs, ablate::solver::Solver &solver, PetscReal stagetime);
-    
-    //public fluxgradvalues to be accessible to prestage
-    std::vector<std::vector<PetscScalar>> fluxGradValues;
-
 };
 }  // namespace ablate::finiteVolume::processes
 #endif
