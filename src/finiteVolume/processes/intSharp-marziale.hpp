a2f652bed9c948f08b972860c7c07cd4f18ae6b3
jjmarzia <jjmarzia@Lalo-6.local>
2025-04-23 14:50:19 -0400

    Parameswaran intsharp implementation

--- src/finiteVolume/processes/intSharp-marziale.hpp
+++ src/finiteVolume/processes/intSharp-marziale.hpp
@@ -20,6 +20,7 @@ class IntSharp : public Process {
     PetscReal Gamma;
     PetscReal epsilon;
     bool flipPhiTilde;
+    bool addtoRHS;
     //mesh for vertex information
     DM vertexDM{};
     std::shared_ptr<ablate::domain::SubDomain> subDomain;
@@ -30,7 +31,7 @@ class IntSharp : public Process {
      * @param Gamma
      * @param epsilon
      */
-    explicit IntSharp(PetscReal Gamma, PetscReal epsilon, bool flipPhiTilde);
+    explicit IntSharp(PetscReal Gamma, PetscReal epsilon, bool flipPhiTilde, bool addtoRHS = false);
 
     /**
      * Clean up the dm created
@@ -55,6 +56,14 @@ class IntSharp : public Process {
      * @return
      */
     static PetscErrorCode ComputeTerm(const FiniteVolumeSolver &solver, DM dm, PetscReal time, Vec locX, Vec locFVec, void *ctx);
+
+    //intsharp prestage stuff
+    inline const static std::string VOLUME_FRACTION_FIELD = eos::TwoPhase::VF;
+    inline const static std::string DENSITY_VF_FIELD = ablate::finiteVolume::CompressibleFlowFields::CONSERVED + VOLUME_FRACTION_FIELD;
+    PetscErrorCode PreStage(TS flowTs, ablate::solver::Solver &solver, PetscReal stagetime);
+    std::vector<std::vector<PetscScalar>> fluxGradValues;
+    std::map<PetscInt, std::vector<PetscInt>> cellNeighbors;
+    std::map<PetscInt, std::vector<PetscInt>> vertexNeighbors;
 };
 }  // namespace ablate::finiteVolume::processes
 #endif
